304b875a3a05be8ef0e8f060fbe75556d0a3670b
Andrew Diggs <andrewrdiggs@gmail.com>
2024-07-23 09:39:20 -0500

    finished the police officer cpp and h

--- ParkingTicketSimulator/PoliceOfficer.cpp
+++ ParkingTicketSimulator/PoliceOfficer.cpp
@@ -1,1 +1,17 @@
 #include "PoliceOfficer.h"
+
+PoliceOfficer::PoliceOfficer(const string& name, int badgeNumber) : name(name), badgeNumber(badgeNumber) {}
+
+ParkingTicket* PoliceOfficer::inspectCar(const ParkedCar& car, const ParkingMeter& meter) const 
+{
+    int illegalMinutes = car.getParkedMinutes() - meter.getPurchasedMinutes();
+    if (illegalMinutes > 0) 
+    {
+        int fine = ParkingTicket::calculateFine(illegalMinutes);
+        return new ParkingTicket(car.getMake(), car.getModel(), car.getColor(), car.getLicenseNumber(), fine, name, badgeNumber);
+    }
+    else 
+    {
+        return nullptr;
+    }
+}
--- ParkingTicketSimulator/PoliceOfficer.h
+++ ParkingTicketSimulator/PoliceOfficer.h
@@ -1,5 +1,23 @@
-#pragma once
-class PoliceOfficer
+#ifndef POLICEOFFICER_H
+#define POLICEOFFICER_H
+
+#include <string>
+#include "ParkedCar.h"
+#include "ParkingMeter.h"
+#include "ParkingTicket.h"
+
+using namespace std;
+
+class PoliceOfficer 
 {
+private:
+    string name;
+    int badgeNumber;
+
+public:
+    PoliceOfficer(const string& name, int badgeNumber);
+
+    ParkingTicket* inspectCar(const ParkedCar& car, const ParkingMeter& meter) const;
 };
 
+#endif // POLICEOFFICER_H
