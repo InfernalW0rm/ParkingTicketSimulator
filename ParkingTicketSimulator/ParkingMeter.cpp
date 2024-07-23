ad97bf7841d8a27929735820f337d38feec8c2e5
Andrew Diggs <andrewrdiggs@gmail.com>
2024-07-23 09:23:12 -0500

    finished parkingmeter .h and .cpp

--- ParkingTicketSimulator/ParkingMeter.cpp
+++ ParkingTicketSimulator/ParkingMeter.cpp
@@ -1,1 +1,8 @@
 #include "ParkingMeter.h"
+
+ParkingMeter::ParkingMeter(int purchasedMinutes) : purchasedMinutes(purchasedMinutes) {}
+
+int ParkingMeter::getPurchasedMinutes() const
+{
+    return purchasedMinutes;
+}
--- ParkingTicketSimulator/ParkingMeter.h
+++ ParkingTicketSimulator/ParkingMeter.h
@@ -1,5 +1,15 @@
-#pragma once
-class ParkingMeter
+#ifndef PARKINGMETER_H
+#define PARKINGMETER_H
+
+class ParkingMeter 
 {
+private:
+    int purchasedMinutes;
+
+public:
+    ParkingMeter(int purchasedMinutes);
+
+    int getPurchasedMinutes() const;
 };
 
+#endif
